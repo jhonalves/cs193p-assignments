0a77814f54cf76a2c82cf6318606555b5de90901
Jhonatan Alves <jhon_fla@hotmail.com>
2021-12-28 13:27:13 -0300

    Adding create cards set function to ViewModel

--- Set/Set/SoloSetGame.swift
+++ Set/Set/SoloSetGame.swift
@@ -10,7 +10,26 @@ import SwiftUI
 class SoloSetGame: ObservableObject {
     typealias Card = SetGame<cardColor, cardShape, CardNumber, cardShading>.Card
     
-    @Published private var model = SetGame<cardColor, cardShape, CardNumber, cardShading>(cardsSet: [])
+    
+    private static func createCardsSet() -> SetGame<cardColor, cardShape, CardNumber, cardShading> {
+        var cardsSet: Array<Card> = []
+        var cardID = 0
+        
+        for color in cardColor.allCases {
+            for shape in cardShape.allCases {
+                for number in CardNumber.allCases {
+                    for shading in cardShading.allCases {
+                        cardsSet.append(Card.init(color: color, shape: shape, quantity: number, shading: shading, id: cardID))
+                        cardID += 1
+                    }
+                }
+            }
+        }
+        
+        return SetGame<cardColor, cardShape, CardNumber, cardShading>(cardsSet: cardsSet)
+    }
+    
+    @Published private var model = createCardsSet()
     
     var cards: Array<Card> {
         model.cards
